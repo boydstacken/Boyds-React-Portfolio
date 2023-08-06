95a6c4b3b36c889a0d3aac0b1c786bc1712207d8
boydstacken <124229670+boydstacken@users.noreply.github.com>
2023-08-05 21:02:23 -0500

    have navbar and header created with more adjustments to come

--- boyds-react-portfolio/src/components/Headers.jsx
+++ boyds-react-portfolio/src/components/Headers.jsx
@@ -1,9 +1,11 @@
 import '../styles/Header.css';
+import '../styles/Navigation.css';
 
 function Header() {
   return (
     <header className="header">
       <h1>Boyd Stacken's Portfolio</h1>
+      <Navigation/>
     </header>
   );
 }
--- boyds-react-portfolio/src/components/Navigation.jsx
+++ boyds-react-portfolio/src/components/Navigation.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
  
+const styles = {
+header: {
+  color: 'white',
+  marginLeft: '0px',
+  padding: '20px',
+  backgroundColor: '#3d3935',
+  display: 'inline-block',
+  fontSize: '32px'
+}
+};
+
 function Navbar() {
     return (
       <div style={styles.card}>
-        <div style={styles.heading}>Home</div>
+        <div style={styles.header}>Boyd Stacken </div>
+        <img src="src/assets/profile pic.jpeg" alt="picture of Boyd Stacken" class="profile-picture"></img>
         <nav>
                 {/* <!--Navbar list items and linked to their section of page. Resume links to PDF doc--> */}
                         <ul>
@@ -12,8 +24,8 @@ function Navbar() {
                             <li><a href="#work">Work</a></li>
                             <li><a href="#about">About</a></li>
                         </ul>
-                    </nav>
-      </div>
+                    </nav>  
+       </div>
     )
   }
   
