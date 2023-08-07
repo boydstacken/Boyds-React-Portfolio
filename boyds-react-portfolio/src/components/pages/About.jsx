21b57cf6e461fddbef75d18371a5ae7efa60a4ea
boydstacken <124229670+boydstacken@users.noreply.github.com>
2023-08-06 22:57:51 -0500

    added h2 title to About section

--- boyds-react-portfolio/src/components/pages/About.jsx
+++ boyds-react-portfolio/src/components/pages/About.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const About = () => {
     return (
         <section className="paragraph=section" id="aboutString">
-        <p>As an asipring Web Developer, I'm driven to understand the needs of users online and create effective web applications to deliver a positive experience. With a background in Marketing, Sales, Communications, & Graphic Design, I'm aiming to connect the consumer experience to the backend of structuring and designing websites. Learning the fundamentals of web development while using my current background will propel me to craft the consumer journey and provide functionality for clients websites. </p>
+        <h2>About Me</h2>
+        <p>As an asipring Web Developer, I'm driven to understand the needs of users online and create effective web applications to deliver a positive experience. With a background in Marketing, Sales, Communications, & Graphic Design, I'm aiming to connect the consumer experience to the backend of structuring and designing websites. Learning the fundamentals of web development while using my current background will propel me to craft the consumer journey and provide functionality for client websites. </p>
         </section>
     )
 }
