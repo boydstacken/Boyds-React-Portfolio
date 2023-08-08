8828656220f80d41e65ed045cc4d03a15dd30466
boydstacken <124229670+boydstacken@users.noreply.github.com>
2023-08-08 17:20:32 -0500

    fixed image pathing issues

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Boyd from '../../assets/Boyd.jpg'
 
 const About = () => {
     return (
         <section className="paragraph=section" id="aboutString">
-        <img src = "src/assets/Boyd.jpg" alt="picture of me"  style={{ height: '200px', width: 'auto' }}
-/>
+        <img src = {Boyd} alt="picture of me"  style={{ height: '200px', width: 'auto' }}
+/> 
         <h2>About Me</h2>
         <p>As an asipring Web Developer, I'm driven to understand the needs of users online and create effective web applications to deliver a positive experience. With a background in Marketing, Sales, Communications, & Graphic Design, I'm aiming to connect the consumer experience to the backend of structuring and designing websites. Learning the fundamentals of web development while using my current background will propel me to craft the consumer journey and provide functionality for client websites. </p>
         </section>
--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -1,4 +1,9 @@
-
+import fitnessforecast from '../../assets/Running-Outside-Banner'
+import characraft from '../../assets/characraft'
+import projectmanagement from '../../assets/Project-Management'
+import graphicdesign from '../../assets/Graphic-Design'
+import sales from '../../assets/sales'
+import photography from '../../assets/Photography'
 
 const Project = () => {
     return (
@@ -8,21 +13,21 @@ const Project = () => {
             <div className="gallery">
               <strong>Fitness Forecast | Project 1</strong>
               <a href="https://boydstacken.github.io/fitness-forecast/">
-                <img src="src/assets/Running-Outside-Banner.png" alt="guy Running" className="flexbox-container img-styling" />
+                <img src={fitnessforecast} alt="guy Running" className="flexbox-container img-styling" />
               </a>
             </div>
             <div className="gallery">
               <strong>CharaCraft | Project 2</strong>
               <a href="https://characraft-27a6d9fbde68.herokuapp.com/">
-                <img src="src/assets/characraft.png" alt="characraft" className="flexbox-container img-styling" />
+                <img src={characraft} alt="characraft" className="flexbox-container img-styling" />
               </a>
             </div>
             <div className="gallery ul">
               <ul>
-                <li><img src="src/assets/Project-Management.png" alt="Project management communication" /></li>
-                <li><img src="src/assets/Graphic-Design.png" alt="Computer showing graphic design" /></li>
-                <li><img src="src/assets/Sales.png" alt="Sales consultation" /></li>
-                <li><img src="src/assets/Photography.png" alt="Picture of dream oasis" /></li>
+                <li><img src={projectmanagement} alt="Project management communication" /></li>
+                <li><img src={graphicdesign} alt="Computer showing graphic design" /></li>
+                <li><img src={sales} alt="Sales consultation" /></li>
+                <li><img src={photography} alt="Picture of dream oasis" /></li>
               </ul>
             </div>
           </div>
