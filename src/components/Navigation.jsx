fa729e6d040a7a003b86c53104683d846875ec3f
boydstacken <124229670+boydstacken@users.noreply.github.com>
2023-08-08 20:03:14 -0500

    fixing profile pic file path

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import profile from '../../assets/profile-pic.jpeg';
+import profile from '../../src/assets/profile-pic.jpeg';
 
 const styles = {
   header: {
